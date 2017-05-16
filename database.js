a80784a8374f7592ea48697dab280ed8eb91c8c8
shane <sdrafahl@iastate.edu>
2017-05-15 22:27:40 -0500

    Refactored get resources post command.

--- database.js
+++ database.js
@@ -39,6 +39,85 @@ method.getUserInformation = function(json_input,cb){
 
 }
 
+method.getResource = function(req,res,cb){
+    console.log("Getting Resource Data");
+    if (req.session.loggedIn) {
+        var id = req.session.resource_id;
+        var connectionString = "SELECT * FROM RESOURCES WHERE ID = " + id;
+        var connection = this.connection;
+        console.log(connectionString);
+        connection.query(connectionString, function(err, rows) {
+            var db_data = rows[0];
+            var data = {
+
+            };
+            console.log("Getting Ratings");
+            var sql = "SELECT * FROM RATINGS WHERE RESOURCE_ID = " + id;
+            console.log(sql);
+            connection.query(sql, function(err, rows_in) {
+                if (err) throw err;
+                sum(rows_in, 0, 0, function(result) {
+                    console.log(db_data.TITLE);
+                    data.rank = result.result;
+                    console.log("result: " + result.result);
+                    data.num_of_commentors = rows.length;
+                    data.title = db_data.TITLE;
+                    data.usrID = rows[0].USER_ID;
+                    data.coords = db_data.COORDS;
+                    data.address = db_data.ADDRESS;
+                    data.type = db_data.TYPE;
+                    data.desc = db_data.USER_DESCRIPTION;
+                    data.city = db_data.CITY;
+                    data.country = db_data.COUNTRY;
+                    data.extension = db_data.EXT;
+                    data.itemID = db_data.ID;
+                    data.currentID = req.session.mYid;
+
+
+                    var sqlString = "SELECT * FROM ACCOUNTS WHERE ID = " + data.usrID;
+                    connection.query(sqlString, function(err, rows) {
+                        data.authorExt = rows[0].PICTURE;
+                        data.authorBio = rows[0].BIO;
+                        data.author = rows[0].USER;
+                        cb(data);
+
+                    });
+
+
+                });
+            });
+        });
+    }
+}
+
+function sum(rows, total, count, cb) {
+
+
+    console.log("count: " + count);
+    console.log("total: " + total);
+    if (count < rows.length) {
+        total = total + rows[count].RATING;
+        sum(rows, total, count + 1, cb);
+    } else {
+        if (rows.length == 0) {
+            var data = {
+                result: -1
+            };
+        } else {
+            var result = Math.round(total / rows.length);
+            console.log("total: " + total);
+            console.log("result: " + result);
+            var data = {
+                result: result
+            };
+        }
+
+
+        return cb(data);
+    }
+
+}
+
 
 
 
--- server.js
+++ server.js
@@ -368,53 +368,9 @@ function sum(rows, total, count, cb) {
 }
 
 router.post("/getResourceData", function(req, res) {
-    console.log("Getting Resource Data");
-    if (req.session.loggedIn) {
-        var id = req.session.resource_id;
-        var connectionString = "SELECT * FROM RESOURCES WHERE ID = " + id;
-        console.log(connectionString);
-        connection.query(connectionString, function(err, rows) {
-            var db_data = rows[0];
-            var data = {
-
-            };
-            console.log("Getting Ratings");
-            var sql = "SELECT * FROM RATINGS WHERE RESOURCE_ID = " + id;
-            console.log(sql);
-            connection.query(sql, function(err, rows_in) {
-                if (err) throw err;
-                sum(rows_in, 0, 0, function(result) {
-                    console.log(db_data.TITLE);
-                    data.rank = result.result;
-                    console.log("result: " + result.result);
-                    data.num_of_commentors = rows.length;
-                    data.title = db_data.TITLE;
-                    data.usrID = rows[0].USER_ID;
-                    data.coords = db_data.COORDS;
-                    data.address = db_data.ADDRESS;
-                    data.type = db_data.TYPE;
-                    data.desc = db_data.USER_DESCRIPTION;
-                    data.city = db_data.CITY;
-                    data.country = db_data.COUNTRY;
-                    data.extension = db_data.EXT;
-                    data.itemID = db_data.ID;
-                    data.currentID = req.session.mYid;
-
-
-                    var sqlString = "SELECT * FROM ACCOUNTS WHERE ID = " + data.usrID;
-                    connection.query(sqlString, function(err, rows) {
-                        data.authorExt = rows[0].PICTURE;
-                        data.authorBio = rows[0].BIO;
-                        data.author = rows[0].USER;
-                        res.json(data);
-
-                    });
-
-
-                });
-            });
-        });
-    }
+    database.getResource(req,res, function(re){
+        res.json(re);
+    });
 });
 
 router.post("/getSessionData", function(req, res) {
